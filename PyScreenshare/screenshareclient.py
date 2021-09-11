435d702f22ba9bbc08570da7fab4b33d7b2599ef
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2021-09-11 08:01:26 -0700

    added folder

---
+++ PyScreenshare/screenshareclient.py
@@ -0,0 +1,38 @@
+import socket
+import pygame
+from threading import Thread
+from zlib import compress
+from mss import mss
+WIDTH = 1900
+HEIGHT = 1000
+def capture_and_send(conn):
+    with mss() as sct:
+        rect = {'top': 0, 'left': 0, 'width': WIDTH, 'height': HEIGHT}
+        while True:
+            img = sct.grab(rect)
+            pixels = compress(img.rgb, 6)
+            size = len(pixels)
+            size_len = (size.bit_length() + 7) // 8
+            conn.send(bytes([size_len]))
+            size_bytes = size.to_bytes(size_len, 'big')
+            conn.send(size_bytes)
+            conn.sendall(pixels)
+
+def main(host='127.0.0.1', port=50000):
+    sock = socket.socket()
+    while True:
+        try:
+            sock.connect((host, port))
+            break
+        except Exception:
+            pass
+    try:
+        while True:
+            thread = Thread(target=capture_and_send, args=(sock,))
+            thread.start()
+            thread.join()
+    except Exception as e:
+        print("ERR: ", e)
+        sock.close()
+if __name__ == '__main__':
+    main()
---
+++ PyScreenshare/screenshareserver.py
@@ -0,0 +1,41 @@
+import socket
+import pygame
+from zlib import decompress
+def recvall(conn, length):
+    buf = b''
+    while len(buf) < length:
+        data = conn.recv(length - len(buf))
+        if not data:
+            return data
+        buf += data
+    return buf
+def main(host='192.168.1.2', port=50000):
+    sock = socket.socket()
+    sock.bind((host, port))
+    print("Listening ....")
+    sock.listen(5)
+    conn, addr = sock.accept()
+    print("Accepted ....", addr)
+    pygame.init()
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    clock = pygame.time.Clock()
+    watching = True
+    try:
+        while watching:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    watching = False
+                    break
+            size_len = int.from_bytes(conn.recv(1), byteorder='big')
+            size = int.from_bytes(conn.recv(size_len), byteorder='big')
+            pixels = decompress(recvall(conn, size))
+            img = pygame.image.fromstring(pixels, (WIDTH, HEIGHT), 'RGB')
+            screen.blit(img, (0, 0))
+            pygame.display.flip()
+            clock.tick(60)
+    finally:
+        print("PIXELS: ", pixels)
+        sock.close()
+
+if __name__ == "__main__":
+    main()
